1397f36b9e113b2eb82e15ce5d37dcd3f11b5a6e
Beauvois Marine <marine.beauvois.pro@hotmail.com>
2022-11-30 15:51:14 +0100

    modification models

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,3 +1,8 @@
 class Shelter < ApplicationRecord
-  has_many :pet
+  has_many :pets
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :address, presence: true
+  # ----------------Vérifier si les deux lignes suivantes fonctionnent avec la view-----------
+  validates :phone_number, presence: true, unless: ->(shelter) { shelter.email.present? }
+  validates :email, presence: true, unless: ->(shelter) { shelter.phone_number.present? }
 end
