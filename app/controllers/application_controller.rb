239e0dae96576ebc4e242769ac903026d7f6e247
Mathieu Garin <mathieugarin08@gmail.com>
2022-12-05 14:30:30 +0100

    Update routes after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,14 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone_number, :employee?])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :phone_number, :employee])
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :phone_number, :employee?])
-    raise
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :phone_number, :employee])
   end
 
 
   # Redirect des users en fonction de #employee? après le formulaire d'inscription
   def after_sign_in_path_for(current_user)
-    current_user.first_name == "employee" ? shelter_path(2) : root_path
+    current_user.employee ? shelter_path(2) : pets_path
   end
 end
