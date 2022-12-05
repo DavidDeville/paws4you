a4cfe2ba8a45f4b3bebf1843250ee363e303a3b7
Gregoire Potaux <gregoire.potaux@gmail.com>
2022-12-05 12:40:16 +0100

    show du refuge dynamique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_084601) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_110817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_084601) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "opening_hour"
+    t.string "closing_hour"
   end
 
   create_table "users", force: :cascade do |t|
