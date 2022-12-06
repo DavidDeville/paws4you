7ce3ca60001f5b8757e61ad3a4a8b2d87bb44231
Mathieu Garin <mathieugarin08@gmail.com>
2022-12-06 16:00:51 +0100

    add pet edit

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,5 @@
 class PetsController < ApplicationController
-
   def index
-
-
     @pets = Pet.all
     @pets = @pets.where("category ILIKE ?", "chien") if current_user.dog_preferences == true
     @pets = @pets.where("category ILIKE ?", "chat") if current_user.cat_preferences == true
@@ -14,5 +11,9 @@ class PetsController < ApplicationController
     current_user.list_pets.each do | list |
       @pets = @pets.where.not(id: list.pet_id)
     end
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
   end
 end
