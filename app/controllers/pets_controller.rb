11f1836008d1c3b54e000085eb39b96e6086a512
David Deville <david.deville59@gmail.com>
2022-12-06 17:50:29 +0100

    list pets features

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,8 +1,6 @@
 class PetsController < ApplicationController
 
   def index
-
-
     @pets = Pet.all
     @pets = @pets.where("category ILIKE ?", "chien") if current_user.dog_preferences == true
     @pets = @pets.where("category ILIKE ?", "chat") if current_user.cat_preferences == true
@@ -14,5 +12,9 @@ class PetsController < ApplicationController
     current_user.list_pets.each do | list |
       @pets = @pets.where.not(id: list.pet_id)
     end
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
   end
 end
