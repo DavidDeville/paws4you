7bce7f9d10057b9f3975277bec8766318a0522dc
Gregoire Potaux <gregoire.potaux@gmail.com>
2022-12-06 17:22:39 +0100

    modification des seed. description des animaux en dynamique

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_134514) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_06_145944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_05_134514) do
     t.boolean "is_adopted"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
