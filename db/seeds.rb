af2ecb16f73cc3f67241e045e2f4a3d97eae3bcc
Gregoire Potaux <gregoire.potaux@gmail.com>
2022-12-07 09:22:28 +0100

    mise en com de la seconde photo des pets

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,73 +56,73 @@ lorem = "Sapristi de tabarnak de mautadit de bâtard de mosus de caltor de charo
     pet1 = Pet.create!(name: "Happy", age: "15/06/2015", race: "Border Collie", gender: "mâle", shelter_id: first_shelter.id, category: "chien", description: lorem, family_friendly: false, is_adopted: false)
     file = URI.open("app/assets/images/Happy1.jpg")
     pet1.photos.attach(io: file, filename: "Happy1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Happy2.jpg")
-    pet1.photos.attach(io: file, filename: "Happy2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Happy2.jpg")
+    # pet1.photos.attach(io: file, filename: "Happy2", content_type: "image/jpg")
     pet1.save
 
     pet2 = Pet.create!(name: "Shenzi", age: "17/07/2017", race: "Boxer", gender: "mâle", shelter_id: first_shelter.id, category: "chien", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Shenzi1.jpg")
     pet2.photos.attach(io: file, filename: "Shenzi1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Shenzi2.jpg")
-    pet2.photos.attach(io: file, filename: "Shenzi2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Shenzi2.jpg")
+    # pet2.photos.attach(io: file, filename: "Shenzi2", content_type: "image/jpg")
     pet2.save
 
     pet3 = Pet.create!(name: "Rexy", age: "06/01/2015", race: "Berger", gender: "femelle", shelter_id: second_shelter.id, category: "chien", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Rexy1.jpg")
     pet3.photos.attach(io: file, filename: "Rexy1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Rexy2.jpg")
-    pet3.photos.attach(io: file, filename: "Rexy2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Rexy2.jpg")
+    # pet3.photos.attach(io: file, filename: "Rexy2", content_type: "image/jpg")
     pet3.save
 
     pet4 = Pet.create!(name: "Joker", age: "26/03/2013", race: "Berger allemand", gender: "mâle", shelter_id: second_shelter.id, category: "chat", description: lorem, family_friendly: false, is_adopted: false)
     file = URI.open("app/assets/images/Joker1.jpg")
     pet4.photos.attach(io: file, filename: "Joker1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Joker2.jpg")
-    pet4.photos.attach(io: file, filename: "Joker2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Joker2.jpg")
+    # pet4.photos.attach(io: file, filename: "Joker2", content_type: "image/jpg")
     pet4.save
 
 
     pet5 = Pet.create!(name: "Aiguillette", age: "22/11/2019", race: "Boxer", gender: "femelle", shelter_id: first_shelter.id, category: "chien", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Aiguillette1.jpg")
     pet5.photos.attach(io: file, filename: "Aiguillette1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Aiguillette2.jpg")
-    pet5.photos.attach(io: file, filename: "Aiguillette2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Aiguillette2.jpg")
+    # pet5.photos.attach(io: file, filename: "Aiguillette2", content_type: "image/jpg")
     pet5.save
 
     pet6 = Pet.create!(name: "Django", age: "09/03/2014", race: "Berger belge Malinois", gender: "mâle", shelter_id: fourth_shelter.id, category: "chien", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Django1.jpg")
     pet6.photos.attach(io: file, filename: "Django1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Django2.jpg")
-    pet6.photos.attach(io: file, filename: "Django2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Django2.jpg")
+    # pet6.photos.attach(io: file, filename: "Django2", content_type: "image/jpg")
     pet6.save
 
     pet7 = Pet.create!(name: "Lola", age: "21/06/2018", race: "Bichon", gender: "femelle", shelter_id: fourth_shelter.id, category: "chien", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Lola1.jpg")
     pet7.photos.attach(io: file, filename: "Lola1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Lola2.jpg")
-    pet7.photos.attach(io: file, filename: "Lola2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Lola2.jpg")
+    # pet7.photos.attach(io: file, filename: "Lola2", content_type: "image/jpg")
     pet7.save
 
     pet8 = Pet.create!(name: "Hagrid", age: "01/01/2020", race: "Berger", gender: "mâle", shelter_id: fourth_shelter.id, category: "chien", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Hagrid1.jpg")
     pet8.photos.attach(io: file, filename: "Hagrid1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Hagrid2.jpg")
-    pet8.photos.attach(io: file, filename: "Hagrid2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Hagrid2.jpg")
+    # pet8.photos.attach(io: file, filename: "Hagrid2", content_type: "image/jpg")
     pet8.save
 
 
     pet9 = Pet.create!(name: "Gamora", age: "20/03/2018", race: "Dogue", gender: "femelle", shelter_id: third_shelter.id, category: "chien", description: lorem, family_friendly: false, is_adopted: false)
     file = URI.open("app/assets/images/Gamora1.jpg")
     pet9.photos.attach(io: file, filename: "Gamora1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Gamora2.jpg")
-    pet9.photos.attach(io: file, filename: "Gamora2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Gamora2.jpg")
+    # pet9.photos.attach(io: file, filename: "Gamora2", content_type: "image/jpg")
     pet9.save
 
     pet10 = Pet.create!(name: "Totor", age: "02/15/2016", race: "Berger belge malinois", gender: "mâle", shelter_id: third_shelter.id, category: "chien", description: lorem, family_friendly: false, is_adopted: false)
     file = URI.open("app/assets/images/Totor1.jpg")
     pet10.photos.attach(io: file, filename: "Totor1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Totor2.jpg")
-    pet10.photos.attach(io: file, filename: "Totor2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Totor2.jpg")
+    # pet10.photos.attach(io: file, filename: "Totor2", content_type: "image/jpg")
     pet10.save
 
   puts "Dogs created"
@@ -134,73 +134,73 @@ lorem = "Sapristi de tabarnak de mautadit de bâtard de mosus de caltor de charo
     pet1 = Pet.create!(name: "Flocon", age: "06/06/2018", race: "Européen", gender: "mâle", shelter_id: fifth_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Flocon1.jpg")
     pet1.photos.attach(io: file, filename: "Flocon1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Flocon2.jpg")
-    pet1.photos.attach(io: file, filename: "Flocon2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Flocon2.jpg")
+    # pet1.photos.attach(io: file, filename: "Flocon2", content_type: "image/jpg")
     pet1.save
 
     pet2 = Pet.create!(name: "Karma", age: "09/09/2019", race: "Européen", gender: "femelle", shelter_id: fifth_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Karma1.jpg")
     pet2.photos.attach(io: file, filename: "Karma1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Karma2.jpg")
-    pet2.photos.attach(io: file, filename: "Karma2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Karma2.jpg")
+    # pet2.photos.attach(io: file, filename: "Karma2", content_type: "image/jpg")
     pet2.save
 
     pet3 = Pet.create!(name: "Titeuf", age: "01/01/2008", race: "Européen", gender: "mâle", shelter_id: fifth_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Titeuf1.jpg")
     pet3.photos.attach(io: file, filename: "Titeuf1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Titeuf2.jpg")
-    pet3.photos.attach(io: file, filename: "Titeuf2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Titeuf2.jpg")
+    # pet3.photos.attach(io: file, filename: "Titeuf2", content_type: "image/jpg")
     pet3.save
 
     pet4 = Pet.create!(name: "Sacha", age: "01/10/2014", race: "Européen", gender: "mâle", shelter_id: fourth_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Sacha1.jpg")
     pet4.photos.attach(io: file, filename: "Sacha1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Sacha2.jpg")
-    pet4.photos.attach(io: file, filename: "Sacha2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Sacha2.jpg")
+    # pet4.photos.attach(io: file, filename: "Sacha2", content_type: "image/jpg")
     pet4.save
 
     pet5 = Pet.create!(name: "Tisha", age: "05/06/2022", race: "Européen", gender: "femelle", shelter_id: fifth_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Tisha1.jpg")
     pet5.photos.attach(io: file, filename: "Tisha1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Tisha2.jpg")
-    pet5.photos.attach(io: file, filename: "Tisha2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Tisha2.jpg")
+    # pet5.photos.attach(io: file, filename: "Tisha2", content_type: "image/jpg")
     pet5.save
 
     pet6 = Pet.create!(name: "Papaya", age: "01/11/2020", race: "Européen", gender: "femelle", shelter_id: first_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Papaya1.jpg")
     pet6.photos.attach(io: file, filename: "Papaya1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Papaya2.jpg")
-    pet6.photos.attach(io: file, filename: "Papaya2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Papaya2.jpg")
+    # pet6.photos.attach(io: file, filename: "Papaya2", content_type: "image/jpg")
     pet6.save
 
     pet7 = Pet.create!(name: "Yoshi", age: "01/06/2022", race: "Européen", gender: "mâle", shelter_id: first_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Yoshi1.jpg")
     pet7.photos.attach(io: file, filename: "Yoshi1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Yoshi2.jpg")
-    pet7.photos.attach(io: file, filename: "Yoshi2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Yoshi2.jpg")
+    # pet7.photos.attach(io: file, filename: "Yoshi2", content_type: "image/jpg")
     pet7.save
 
 
     pet8 = Pet.create!(name: "Musti", age: "01/12/2021", race: "Européen", gender: "femelle", shelter_id: second_shelter.id, category: "chat", description: lorem,  family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Musti1.jpg")
     pet8.photos.attach(io: file, filename: "Musti1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Musti2.jpg")
-    pet8.photos.attach(io: file, filename: "Musti2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Musti2.jpg")
+    # pet8.photos.attach(io: file, filename: "Musti2", content_type: "image/jpg")
     pet8.save
 
     pet9 = Pet.create!(name: "Diablo", age: "23/12/2015", race: "Européen", gender: "mâle", shelter_id: second_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Diablo1.jpg")
     pet9.photos.attach(io: file, filename: "Diablo1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Diablo2.jpg")
-    pet9.photos.attach(io: file, filename: "Diablo2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Diablo2.jpg")
+    # pet9.photos.attach(io: file, filename: "Diablo2", content_type: "image/jpg")
     pet9.save
 
 
     pet10 = Pet.create!(name: "Rouka", age: "25/07/2022", race: "Européen", gender: "mâle", shelter_id: third_shelter.id, category: "chat", description: lorem, family_friendly: true, is_adopted: false)
     file = URI.open("app/assets/images/Rouka1.jpg")
     pet10.photos.attach(io: file, filename: "Rouka1", content_type: "image/jpg")
-    file = URI.open("app/assets/images/Rouka2.jpg")
-    pet10.photos.attach(io: file, filename: "Rouka2", content_type: "image/jpg")
+    # file = URI.open("app/assets/images/Rouka2.jpg")
+    # pet10.photos.attach(io: file, filename: "Rouka2", content_type: "image/jpg")
     pet10.save
 
     puts "Cats created"
