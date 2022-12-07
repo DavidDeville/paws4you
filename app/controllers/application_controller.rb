e63ff98f28fc89965a08a05505de526a42810b76
David Deville <david.deville59@gmail.com>
2022-12-07 22:33:16 +0100

    first test of meta data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,8 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(current_user)
     conditions_path
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
