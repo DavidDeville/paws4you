022f04d30fe911f31a909f42a193e32c61fd62a3
Gregoire Potaux <gregoire.potaux@gmail.com>
2022-12-08 14:42:43 +0100

    alerte mise en place

--- app/javascript/controllers/fireworks_controller.js
+++ app/javascript/controllers/fireworks_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
-import { Fireworks } from 'fireworks-js'
-
-
-
+// import { Fireworks } from 'fireworks-js'
+import swal from 'sweetalert';
 
 
 // Connects to data-controller="fireworks"
@@ -12,62 +10,71 @@ export default class extends Controller {
   static targets = ['firebtn']
 
   connect() {
-    console.log("fireworks!")
+
+   console.log(this.firebtnTarget)
+    // console.log("fireworks!")
   }
 
-  test() {
-    const resource = document.querySelector('.fire')
-    const fireworks = new Fireworks(resource, {
-      autoresize: true,
-      opacity: 1,
-      acceleration: 1.05,
-      friction: 0.97,
-      gravity: 1.5,
-      particles: 50,
-      traceLength: 3,
-      traceSpeed: 10,
-      explosion: 5,
-      intensity: 50,
-      flickering: 50,
-      hue: {
-        min: 0,
-        max: 360
-      },
-      delay: {
-        min: 30,
-        max: 60
-      },
-      rocketsPoint: {
-        min: 50,
-        max: 50
-      },
-      lineWidth: {
-        explosion: {
-          min: 1,
-          max: 3
-        },
-        trace: {
-          min: 1,
-          max: 2
-        }
-      },
-      brightness: {
-        min: 50,
-        max: 80
-      },
-      decay: {
-        min: 0.015,
-        max: 0.03
-      },
-      mouse: {
-        click: false,
-        move: false,
-        max: 1
-      }
-    })
-    this.firebtnTarget.innerHTML = "Demande envoyée !";
-    setInterval(() => {
-      fireworks.start()
-    }, 1000);
+  alert(){
+    swal("Demande envoyée", "Le refuge a bien reçu votre demande et vous répondra au plus vite!", "success");
+
+
   }
+
+
+  // test() {
+    // const resource = document.querySelector('.fire')
+    // const fireworks = new Fireworks(resource, {
+    //   autoresize: true,
+    //   opacity: 1,
+    //   acceleration: 1.05,
+    //   friction: 0.97,
+    //   gravity: 1.5,
+    //   particles: 50,
+    //   traceLength: 3,
+    //   traceSpeed: 10,
+    //   explosion: 5,
+    //   intensity: 50,
+    //   flickering: 50,
+    //   hue: {
+    //     min: 0,
+    //     max: 360
+    //   },
+    //   delay: {
+    //     min: 30,
+    //     max: 60
+    //   },
+    //   rocketsPoint: {
+    //     min: 50,
+    //     max: 50
+    //   },
+    //   lineWidth: {
+    //     explosion: {
+    //       min: 1,
+    //       max: 3
+    //     },
+    //     trace: {
+    //       min: 1,
+    //       max: 2
+    //     }
+    //   },
+    //   brightness: {
+    //     min: 50,
+    //     max: 80
+    //   },
+    //   decay: {
+    //     min: 0.015,
+    //     max: 0.03
+    //   },
+    //   mouse: {
+    //     click: false,
+    //     move: false,
+    //     max: 1
+    //   }
+    // })
+    // this.firebtnTarget.innerHTML = "Demande envoyée !";
+    // setInterval(() => {
+    //   fireworks.start()
+    // }, 1000);
+  // }
 }
