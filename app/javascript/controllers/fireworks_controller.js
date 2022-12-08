af71d8196d122cbb732638d6a25cc90d9335cc94
David Deville <david.deville59@gmail.com>
2022-12-08 01:39:01 +0100

    fixed fireworks

--- app/javascript/controllers/fireworks_controller.js
+++ app/javascript/controllers/fireworks_controller.js
@@ -2,55 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 import { Fireworks } from 'fireworks-js'
 
-const container = document.querySelector('.fire')
-const fireworks = new Fireworks(container, {
-  autoresize: true,
-  opacity: 1,
-  acceleration: 1.05,
-  friction: 0.97,
-  gravity: 1.5,
-  particles: 50,
-  traceLength: 3,
-  traceSpeed: 10,
-  explosion: 5,
-  intensity: 50,
-  flickering: 50,
-  hue: {
-    min: 0,
-    max: 360
-  },
-  delay: {
-    min: 30,
-    max: 60
-  },
-  rocketsPoint: {
-    min: 50,
-    max: 50
-  },
-  lineWidth: {
-    explosion: {
-      min: 1,
-      max: 3
-    },
-    trace: {
-      min: 1,
-      max: 2
-    }
-  },
-  brightness: {
-    min: 50,
-    max: 80
-  },
-  decay: {
-    min: 0.015,
-    max: 0.03
-  },
-  mouse: {
-    click: false,
-    move: false,
-    max: 1
-  }
-})
+
+
 
 
 // Connects to data-controller="fireworks"
@@ -60,10 +13,58 @@ export default class extends Controller {
 
   connect() {
     console.log("fireworks!")
-
   }
 
   test() {
+    const resource = document.querySelector('.fire')
+    const fireworks = new Fireworks(resource, {
+      autoresize: true,
+      opacity: 1,
+      acceleration: 1.05,
+      friction: 0.97,
+      gravity: 1.5,
+      particles: 50,
+      traceLength: 3,
+      traceSpeed: 10,
+      explosion: 5,
+      intensity: 50,
+      flickering: 50,
+      hue: {
+        min: 0,
+        max: 360
+      },
+      delay: {
+        min: 30,
+        max: 60
+      },
+      rocketsPoint: {
+        min: 50,
+        max: 50
+      },
+      lineWidth: {
+        explosion: {
+          min: 1,
+          max: 3
+        },
+        trace: {
+          min: 1,
+          max: 2
+        }
+      },
+      brightness: {
+        min: 50,
+        max: 80
+      },
+      decay: {
+        min: 0.015,
+        max: 0.03
+      },
+      mouse: {
+        click: false,
+        move: false,
+        max: 1
+      }
+    })
     this.firebtnTarget.innerHTML = "Demande envoyée !";
     setInterval(() => {
       fireworks.start()
